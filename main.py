ae6c42d3fcc9d57b0fa5ac3ed3f48a3f3552d157
Win Htaik Aung <winhtaikaung28@hotmail.com>
2024-02-14 13:32:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def apply_ocr(cell_coordinates,image):
       if len(row_text) > max_num_columns:
           max_num_columns = len(row_text)
 
-      data[idx] = list(map(lambda x: str(x).lower().replace('v','').replace(' ','').replace('^',''), row_text))
+      data[idx] = list(map(lambda x: str(x).replace('v','').replace('V','').replace(' ','').replace('^',''), row_text))
 
     print("Max number of columns:", max_num_columns)
 
